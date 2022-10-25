94d655dee6017f787cb61ba6da41cac24f39736f
Khankamol Chor Kongrukgreatiyos <khankamolk@berkeley.edu>
2022-10-24 21:58:38 -0700

    finished

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,16 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
+import Color from './Color'
 
-const Menu = () => { 
+const Menu = (props) => { 
+  const update = props.click;
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="red" handleClick={update}></Color>
+          <Color color="pink" handleClick={update}></Color>
+          <Color color="blue" handleClick={update}></Color>
+          <Color color="green" handleClick={update}></Color>
       </div>
     );
 }
